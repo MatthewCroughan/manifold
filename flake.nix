9a975d58f257a27dc883d1e999e3cb03eef1800a
matthewcroughan <matt@croughan.sh>
2023-05-27 20:56:58 +0100

    wip

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  # 22.11 does not include PR #218472, hence we use the unstable version
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+  inputs.fenix.url = github:nix-community/fenix;
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      name = "manifold";
+      pkgs = system: import nixpkgs {
+        inherit system;
+      };
+      shell = pkgs: pkgs.mkShell {
+        inputsFrom = [ self.packages.${pkgs.system}.default ];
+
+        # ---- START package specific dev settings ----
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+        # ---- END package specific dev settings ----
+      };
+      package = pkgs:
+        let
+          toolchain = fenix.packages.${pkgs.system}.minimal.toolchain;
+        in
+          (pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          }).buildRustPackage rec {
+            pname = "stardust-xr-${name}";
+            src = ./.;
+
+            # ---- START package specific settings ----
+            version = "0.10.2";
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              allowBuiltinFetchGit = true;
+            };
+
+            CPM_SOURCE_CACHE = "./build";
+
+            nativeBuildInputs = with pkgs; [
+              cmake pkg-config llvmPackages.libcxxClang
+            ];
+
+            buildInputs = with pkgs; [
+              openxr-loader libGL mesa xorg.libX11 fontconfig libxkbcommon
+              xorg.libXcursor xorg.libXrandr xorg.libXi
+            ];
+
+            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            # ---- END package specific settings ----
+          };
+    in
+    {
+      overlays.default = final: prev: {
+        stardust-xr = (prev.stardust-xr or {}) // {
+          ${name} = package final;
+        };
+      };
+
+      packages."x86_64-linux".default = package (pkgs "x86_64-linux");
+      packages."aarch64-linux".default = package (pkgs "aarch64-linux");
+
+      devShells."x86_64-linux".default = shell (pkgs "x86_64-linux");
+      devShells."aarch64-linux".default = shell (pkgs "aarch64-linux");
+    };
+}
+
